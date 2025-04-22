5c9f6115c0138fbe28ab0e0a9d41d60b47340caf
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-04-22 19:12:23 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <stack>
+#include <vector>
+
+int solution(std::string skill, std::vector<std::string> skill_trees) 
+{
+    int answer = 0;
+    std::vector<std::string> SkillTrist;
+    std::string ConfirmedStr;
+
+    for (auto s : skill_trees)
+    {
+        ConfirmedStr.clear();
+        for (auto t : s)
+        {
+            // 유저의 스킬트리가 skill에 있는 항목들인가 검사
+            if (skill.find(t) != std::string::npos)
+            {
+                // skill에 있는 것들만 따로 담는다.
+                ConfirmedStr += t;
+            }
+        }
+        if (skill.substr(0, ConfirmedStr.size()) == ConfirmedStr)
+        {
+            answer++;
+        }
+    }
+    return answer;
+}
